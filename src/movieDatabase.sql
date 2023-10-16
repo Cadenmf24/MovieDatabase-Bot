98377d5e6e3f77e6c404d6da40a6aea33e54c759
Cadenmf24 <cadenmf24@gmail.com>
2023-10-16 12:16:13 -0400

    database works hopefully

---
+++ src/movieDatabase.sql
@@ -0,0 +1,8 @@
+CREATE TABLE movies(
+    id  SERIAL PRIMARY KEY NOT NULL,
+    movieName TEXT NOT NULL,
+    releaseDate TIMESTAMP, 
+);
+
+INSERT INTO movies(movieName, releaseDate) VALUES
+('The Untold Truth', '2023-01-01');
